5c38373660ef2ae2d7acc392ebf4040e21dcace9
Nathaniel Meyer and Stormi Hoebelheinrich <stormi@>
2017-03-23 14:00:21 -0700

    Add spark routes and templates for heroes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -45,5 +45,38 @@ public class App {
     return new ModelAndView(model, layout);
   }, new VelocityTemplateEngine());
 
+  get("/squads/:id/heroes/new", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+    model.put("squad", squad);
+    model.put("template", "templates/squad-heroes-form.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  post("/squads/:id/heroes", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    Squad squad = Squad.find(Integer.parseInt(request.params(":id")));
+    String heroName = request.queryParams("heroName");
+    String secretIdentity = request.queryParams("secretIdentity");
+    String superPower = request.queryParams("superPower");
+    String weakness = request.queryParams("weakness");
+    int strengthValue = Integer.parseInt(request.queryParams("strengthValue"));
+    int intelValue = Integer.parseInt(request.queryParams("intelValue"));
+    Hero newHero = new Hero(heroName, secretIdentity, superPower, weakness, strengthValue, intelValue);
+    squad.addHero(newHero);
+    model.put("squad", squad);
+    model.put("template", "templates/squad-heroes-success.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
+
+  get("/squads/:id1/heroes/:id2", (request, response) -> {
+    Map<String, Object> model = new HashMap<String, Object>();
+    Squad squad = Squad.find(Integer.parseInt(request.params(":id1")));
+    Hero hero = Hero.find(Integer.parseInt(request.params(":id2")));
+    model.put("squad", squad);
+    model.put("hero", hero);
+    model.put("template", "templates/hero.vtl");
+    return new ModelAndView(model, layout);
+  }, new VelocityTemplateEngine());
   }
 }
